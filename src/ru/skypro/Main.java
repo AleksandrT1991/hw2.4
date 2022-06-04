00738033da20fc5b51d4cfce137bb2d35288513c
AleksandrTsygulev <aleksandr.tsygulev@gmail.com>
2022-06-04 18:05:02 +0300

    Initial commit

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,14 @@
+package ru.skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean result = Validator.check("test_1234", "D_1hWiKjjP_9", "D_1hWiKjjP_8" );
+        if (result) {
+            System.out.println("Логин и пароль корректные");
+        }else {
+            System.out.println("Логин и/или пароль некорректный");
+        }
+    }
+}
